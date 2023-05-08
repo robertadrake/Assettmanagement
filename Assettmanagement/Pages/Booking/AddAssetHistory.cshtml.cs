775d86da0408dfba83f975d8259a053aa0e63817
Rob D <robert@dtwins.co.uk>
2023-05-08 12:09:46 +0100

    working now adding securty

--- Assettmanagement/Pages/Booking/AddAssetHistory.cshtml.cs
+++ Assettmanagement/Pages/Booking/AddAssetHistory.cshtml.cs
@@ -14,7 +14,8 @@ namespace Assettmanagement.Pages.Booking
     {
         private readonly DataAccess _dataAccess;
         public List<AssetHistory> AssetHistories { get; set; }
-
+        [BindProperty(SupportsGet = true)]
+        public string SelectedAssetType { get; set; }
         public AddAssetHistoryModel(DataAccess dataAccess)
         {
             _dataAccess = dataAccess;
@@ -38,10 +39,20 @@ namespace Assettmanagement.Pages.Booking
             var assets = await _dataAccess.GetAssetsAsync();
             var users = await _dataAccess.GetUsersAsync();
 
-            ViewData["AssetList"] = new SelectList(assets, "Id", "Name");
-            ViewData["UserList"] = new SelectList(users, "Id", "FirstName");
+            if (!string.IsNullOrEmpty(SelectedAssetType))
+            {
+                assets = assets.Where(a => a.AssetType == SelectedAssetType).ToList();
+            }
 
-            if (assets.Any())
+            ViewData["AssetList"] = assets.Select(a => new SelectListItem
+            {
+                Value = a.Id.ToString(),
+                Text = a.SerialNumber
+            }).ToList();
+
+            ViewData["AssetList"] = new SelectList(assets, "Id", "SerialNumber"); // Changed from "Name" to "SerialNumber"
+            ViewData["UserList"] = new SelectList(users.Select(u => new { u.Id, FullName = $"{u.FirstName} {u.LastName}" }), "Id", "FullName");
+
             {
                 await LoadAssetHistoriesAsync(assets.First().Id);
             }
@@ -49,6 +60,11 @@ namespace Assettmanagement.Pages.Booking
             return Page();
         }
 
+        public async Task<IActionResult> OnGetGetAssetDetailsAsync(int assetId)
+        {
+            var asset = await _dataAccess.GetAssetAsync(assetId);
+            return new JsonResult(asset);
+        }
 
         public async Task<IActionResult> OnPostAsync()
         {
