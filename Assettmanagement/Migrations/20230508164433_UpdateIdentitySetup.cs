90372ebead49361e26b323b128431782f12806b7
Rob D <robert@dtwins.co.uk>
2023-07-09 16:59:18 +0100

    no idea whats going on

---
+++ Assettmanagement/Migrations/20230508164433_UpdateIdentitySetup.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Assettmanagement.Migrations
+{
+    public partial class UpdateIdentitySetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserInfoId",
+                table: "AspNetUsers",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_UserInfoId",
+                table: "AspNetUsers",
+                column: "UserInfoId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Users_UserInfoId",
+                table: "AspNetUsers",
+                column: "UserInfoId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Users_UserInfoId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_UserInfoId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "UserInfoId",
+                table: "AspNetUsers");
+        }
+    }
+}
---
+++ Assettmanagement/Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Assettmanagement.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public int? UserInfoId { get; set; }
+        public User UserInfo { get; set; }
+    }
+
+}
